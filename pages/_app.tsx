3c85f047adcabc4307c6fa334238aa4dffe775eb
Solo Shun <88045720+soloeinsteinmit@users.noreply.github.com>
2025-12-26 06:56:20 +0000

    feat: Add Vercel Speed Insights and global app setup with Space Grotesk font.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Space_Grotesk } from "next/font/google";
+import { useRouter } from "next/router";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -8,9 +10,12 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <div id="root" className={spaceGrotesk.className}>
       <Component {...pageProps} />
+      <SpeedInsights route={router.pathname} />
     </div>
   );
 }
