5629d7b2d770564e5ae2f986b8a4662a4980e82e
Solo Shun <88045720+soloeinsteinmit@users.noreply.github.com>
2025-12-26 07:22:29 +0000

    feat: add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { Space_Grotesk } from "next/font/google";
 import { useRouter } from "next/router";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from '@vercel/analytics/react';
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div id="root" className={spaceGrotesk.className}>
       <Component {...pageProps} />
       <SpeedInsights route={router.pathname} />
+      <Analytics />
     </div>
   );
 }
